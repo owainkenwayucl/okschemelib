db2f1ee034113473a2396269644964e4f84ab517
Owain Kenway <o.kenway@ucl.ac.uk>
2018-11-01 10:26:40 +0000

    Revert "Remove ok prefix"
    This reverts commit c1cfd40fea30533c6dcd9acfb722df1c9f227436.

--- oklib.scm
+++ oklib.scm
@@ -1,30 +1,30 @@
 ; Library of Scheme functions for when they just aren't there.
 
 ; range begin end step
-(define (range b n j) 
+(define (okrange b n j) 
 	(if (>= b n) 
 		'() 
-		(cons b (range (+ b j) n j))
+		(cons b (okrange (+ b j) n j))
 	)
 )
 
 ; map function list
-(define (map f d) 
+(define (okmap f d) 
 	(if (= (length d) 0) 
 		d 
-		(cons (f (car d)) (map f (cdr d)))
+		(cons (f (car d)) (okmap f (cdr d)))
 	)
 )
 
 ; check to see if string s ends with string e
-(define (endswith? s e)
+(define (okendswith? s e)
 	(if (> (string-length s) (string-length e))
 		(string=? e (substring s (- (string-length s) (string-length e)) (string-length s) ))
 	)
 )
 
 ; check to see if string s ends with string e (case insensitive)
-(define (endswith-ci? s e)
+(define (okendswith-ci? s e)
 	(if (> (string-length s) (string-length e))
 		(string-ci=? e (substring s (- (string-length s) (string-length e)) (string-length s) ))
 	)
